783da6da0d8a459519f7d70dae887612c663a084
Swmarly <69097826+Swmarly@users.noreply.github.com>
2026-09-12 12:14:47 +0200

    Bump Seed Totem port to 4.3.6

--- SeedTotemMod.cs
+++ SeedTotemMod.cs
@@ -26,7 +26,7 @@ namespace SeedTotem
     {
         public const string PluginGUID = "marcopogo.SeedTotem";
         public const string PluginName = "Swmarly Valheim Seed Totem";
-        public const string PluginVersion = "4.3.5";
+        public const string PluginVersion = "4.3.6";
         private SeedTotemPrefabConfig seedTotemPrefabConfig;
         private Harmony harmony; 
 
